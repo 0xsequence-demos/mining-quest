4c655bf987ab877c1aaff08c9efe2e3572afc32a
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2025-02-26 14:36:02 +0300

    Preload wallet img

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
 
+// Add preload link for wallet app logo
+const preloadLink = document.createElement("link");
+preloadLink.rel = "preload";
+preloadLink.as = "image";
+preloadLink.href = import.meta.env.VITE_WALLET_APP_LOGO;
+document.head.appendChild(preloadLink);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
